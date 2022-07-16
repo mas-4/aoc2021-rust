61082394655e16cab490162459946d6ce3b9b7dc
mas <m@stdwtr.io>
2022-07-16 11:08:59 -0400

    aoc1 done

--- src/aoc1.rs
+++ src/aoc1.rs
@@ -1,12 +1,17 @@
 use std::fs;
 
-pub fn aoc1() {
+fn read_input() -> Vec<i32> {
     let contents = fs::read_to_string("input/aoc1.txt").expect( "Couldn't open." );
     let numbers = contents
         .trim()
         .split("\n")
         .map(|x| x.parse::<i32>().unwrap())
         .collect::<Vec<i32>>();
+    numbers
+}
+
+pub fn aoc1_1() {
+    let numbers = read_input();
     let mut increased = 0;
     let mut last = numbers[0];
     for i in numbers[1..].iter() {
@@ -15,5 +20,24 @@ pub fn aoc1() {
         }
         last = *i;
     }
-    println!("{}", increased);
+    println!("AOC1.1: {}", increased);
+}
+
+pub fn aoc1_2() {
+    let numbers = read_input();
+    let mut first = numbers[0];
+    let mut second = numbers[1];
+    let mut third = numbers[2];
+    let mut sum = first + second + third;
+    let mut increased = 0;
+    for i in numbers[3..].iter() {
+        if second + third + i > sum {
+            increased += 1;
+        }
+        first = second;
+        second = third;
+        third = *i;
+        sum = first + second + third;
+    }
+    println!("AOC1.2: {}", increased);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod aoc1;
 
 fn main() {
-    aoc1::aoc1();
+    aoc1::aoc1_1();
+    aoc1::aoc1_2();
 }
