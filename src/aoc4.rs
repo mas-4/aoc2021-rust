7485b8556e89d03ab960dafc737873c384522ac0
mas <m@stdwtr.io>
2022-07-17 15:15:45 -0400

    4.1

---
+++ src/aoc4.rs
@@ -0,0 +1,101 @@
+use std::fs;
+use std::fmt;
+
+// can only be a square
+struct Board {
+    board: Vec<u32>,
+    marked: Vec<bool>,
+    size: usize,
+}
+
+impl Board {
+    fn build(input: &[&str]) -> Self {
+        let size = input.len();
+        let marked = vec![false; size * size];
+        let input = input.join(" ");
+        let board = input
+            .split_whitespace()
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+        Board {
+            board,
+            marked,
+            size,
+        }
+    }
+    fn mark(&mut self, num: u32) {
+        for i in 0..(self.size*self.size) {
+            if self.board[i] == num {
+                self.marked[i] = true;
+            }
+        }
+    }
+    fn bingo(&self) -> bool {
+        for i in 0..self.size {
+            let row = self.marked[i * self.size..(i + 1) * self.size].to_vec();
+            let col = vec![
+                self.marked[i * self.size],
+                self.marked[i * self.size + 1],
+                self.marked[i * self.size + 2],
+                self.marked[i * self.size + 3],
+                self.marked[i * self.size + 4],
+            ];
+            if row.iter().all(|x| *x) || col.iter().all(|x| *x) {
+                return true;
+            }
+        }
+        false
+    }
+    fn sum(&self) -> u32 {
+        let mut sum = 0;
+        for (i, val) in self.board.iter().enumerate() {
+            if !self.marked[i] {
+                sum += val;
+            }
+        }
+        sum
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for i in 0..self.size {
+            for j in 0..self.size {
+                if self.marked[i * self.size + j] {
+                    write!(f, " X ")?;
+                } else {
+                    write!(f, " - ",)?;
+                }
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
+    }
+}
+
+fn read_input() -> (Vec<Board>, Vec<u32>) {
+    let contents = fs::read_to_string("input/aoc4.txt").expect( "Couldn't open." );
+    let input: Vec<&str> = contents
+        .lines()
+        .filter(|x| x.len() > 0)
+        .collect();
+    let numbers: Vec<u32> = input[0]
+        .split(',')
+        .map(|x| x.parse::<u32>().unwrap())
+        .collect();
+    let boards = input[1..].chunks(5).map(|x| Board::build(x)).collect::<Vec<Board>>();
+    (boards, numbers)
+}
+
+pub fn aoc4_1() {
+    let (mut boards, nums) = read_input();
+    for num in nums {
+        for board in boards.iter_mut() {
+            board.mark(num);
+            if board.bingo() {
+                println!("AOC4.1: {}", board.sum() * num);
+                return;
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod aoc1;
 mod aoc2;
 mod aoc3;
+mod aoc4;
 
 fn main() {
     aoc1::aoc1_1();
@@ -9,4 +10,5 @@ fn main() {
     aoc2::aoc2_2();
     aoc3::aoc3_1();
     aoc3::aoc3_2();
+    aoc4::aoc4_1();
 }
